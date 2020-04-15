06ce8b3ec799d92f2346ce1173d222a8349a21c1
cjw0816 <394189874@qq.com>
2020-04-16 01:11:39 +0800

    Update Input.java

--- src/Input.java
+++ src/Input.java
@@ -1,12 +1,52 @@
+import java.util.Scanner;
+
 public class Input {
     /**
      * 显示提示，提醒用户输入
      * 返回获取的用户输入字符串
      *
-     * @return
      */
-    public static String input() {
-
+    public static String Input(String[] args) {
+        System.out.println("请输入需要计算的整数（如：1+10）:");
+        Scanner sc = new Scanner(System.in);
+        String info = sc.nextLine();
+        if (!info.isEmpty()){
+            if(info.contains("+")) {
+                if(infojudge(info,"\\+")=="yes"){
+                    return info;
+                }
+            }
+            else if(info.contains("-")){
+                if(infojudge(info,"\\-")=="yes"){
+                    return info;
+                }
+            }
+            else if(info.contains("*")){
+                if(infojudge(info,"\\*")=="yes"){
+                    return info;
+                }
+            }
+            else if (info.contains("/")){
+                if(infojudge(info,"\\/")=="yes"){
+                    return info;
+                }
+            }
+            else {
+                System.out.println("未输入符号");
+                return null;
+            }
+        }
+        System.out.println("未输入");
         return null;
+    }
+    static String infojudge(String info, String symbol){
+        String[] infos = info.split(symbol);
+        if (infos.length == 2 && (!infos[0].isEmpty() && !infos[1].isEmpty())) {
+            return "yes";
+        }
+        else {
+            System.out.println("未输入两位数");
+            return null;
+        }
     }
 }
