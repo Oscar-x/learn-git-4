4ae4de33db66edaaee51a9f9ddbb03fe232a279f
TieJiang13 <1226959428@qq.com>
2020-04-15 22:18:50 +0800

    update Subtract.java

--- src/Subtract.java
+++ src/Subtract.java
@@ -5,6 +5,12 @@ public class Subtract {
      * @return
      */
     public static int subtract() {
-        return 0;
+        Scanner sc new Scanner(System.in);
+	int a,b,sum=0;
+	a=sc.nextInt();
+	b=sc.nextInt();
+	sum=n1-n2;
+	System.out.println("两数相减等于“+sum);
+	return 0;
     }
 }
