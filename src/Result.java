bc99728fb1f43951d013ed6ddc90bc7dc3fa5d94
BlueNeet <w1547220803@gmail.com>
2020-04-15 22:26:53 +0800

    syuiz

--- src/Result.java
+++ src/Result.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Result {
     /**
      * 传入的是一个字符串 类似“1+1” “10+12”
@@ -8,7 +9,27 @@ public class Result {
      *
      * @return
      */
-    public static int result() {
-        return 0;
-    }
+    public static int result(string x) {
+        String count = "114+514";
+        String[] num = count.split("\\D");
+        String[] symbol=count.split("\\d+");
+        String sym=symbol[1];      
+        System.out.printf("%s\n",sym);
+        int a=Integer.valueOf(num[0]).intValue();
+        int b=Integer.valueOf(num[1]).intValue();
+        if(sym=="+"){
+            Add.add(a,b);
+        }
+        else if(sym=="-"){
+            Subtract.subtract(a,b);
+        }
+        else if(sym=="*"){
+            Multiply.multiply(a, b);
+        }
+        else if(sym=="/"){
+            Divide.divide(a,b);
+        }
+        else{
+            return null;
+        }
 }
